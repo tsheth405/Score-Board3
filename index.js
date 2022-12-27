30bd7353068f9493d3a32a5ed95d8b8e9dddcb4e
tsheth40 <sheth1226trisha@gmail.com>
2022-12-27 16:38:08 -0600

    initial files

---
+++ index.js
@@ -0,0 +1,38 @@
+let scoreEl = document.getElementById("score-el")
+let scoreEl2 = document.getElementById("score-el2")
+
+count = 0
+function increaseByOne(){
+    count += 1
+    scoreEl.textContent = count
+}
+
+function increaseByTwo(){
+    count += 2
+    scoreEl.textContent = count
+}
+
+function increaseByThree(){
+    count += 3
+    scoreEl.textContent = count
+}
+
+count2 = 0
+function increaseByOne2(){
+    count2 += 1
+    scoreEl2.textContent = count2
+}
+
+function increaseByTwo2(){
+    count2 += 2
+    scoreEl2.textContent = count2
+}
+
+function increaseByThree2(){
+    count2 += 3
+    scoreEl2.textContent = count2
+}
+
+
+
+
